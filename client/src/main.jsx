f42e05e65564a9a49a46f646db9b5f9f4e5c0d75
tryamb <tryambsachan69@gmail.com>
2024-03-27 00:10:47 +0530

    added

---
+++ client/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Pages from './Pages.jsx'
+import App from './App.jsx'
+import Login from './Login.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Login/>
+ </React.StrictMode>
+)
